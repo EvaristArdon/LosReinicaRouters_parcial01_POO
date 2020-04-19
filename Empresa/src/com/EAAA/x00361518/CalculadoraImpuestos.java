08ac899ee9754effedf7bf951a870bd1942d9cb5
maanuhb <00094119@uca.edu.sv>
2020-04-18 20:43:34 -0600

    Modify: CalculardoraImpuestos

--- Empresa/src/com/EAAA/x00361518/CalculadoraImpuestos.java
+++ Empresa/src/com/EAAA/x00361518/CalculadoraImpuestos.java
@@ -1,4 +1,24 @@
 package com.EAAA.x00361518;
 
-public class CalculadoraImpuestos {
+public final class CalculadoraImpuestos {
+    private double totalRenta;
+    private double totalISSS;
+    private double totalAFP;
+
+
+private CalculadoraImpuestos(){
+
+}
+public double cacularPago(Empleado pago){
+    //esto es para que no de error papu
+return cacularPago(pago);
+
+
+}
+public String mostrarTotales(){
+    //x2
+    String m="";
+    return m;
+}
+
 }
